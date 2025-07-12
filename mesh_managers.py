5f69abe59c1a83fa15895620e5edc33cd1042bac
ab8oj <e.e.thierbach@gmail.com>
2025-07-11 23:26:56 -0400

    Add MeshManager and scan methods

--- mesh_managers.py
+++ mesh_managers.py
@@ -1,25 +1,81 @@
-# TODO: This will be where the "logic layer" lives, that can be used by a number of higher-level
-#       Python applications, whether simple scripts or more complex stuff
-#       Try to make it add value to the meshtastic package.
+# TODO: Add sane logging
 
-"""
-Ideas for stuff to add here:
-in general, move interface management here, leaving the apps to do simple things like send text
-    and display node info.
-    class interfaceManager
-        interface - maybe a dict if we support multiple interfaces - all the active interfaces
-            then a caller can do something like: im=interfaceManager(), im.interface["OJB1"].sendText("foo")
-            include "connected" key to show connection state (take that, global variable!)
-        interface_types - list of supported interface types e.g. ["ble", "rest", "serial"]
-        methods for:
-            getting all available interfaces (optionally a specific type)
-            connect to a specific interface (pass in what, the address or short name or ?)
-            connect to the first available device on a given interface type (might only work for ble?)
-            connect to the first available device on all interface types, given a specific order of types
+import ble
+import logging
+from pubsub import pub
 
-"""
+class InterfaceError(Exception):
+    pass
 
-"""
-Anti-ideas: what *shouldn't* go here
-App layer should probably be the one to do the subscribing to the topics
-"""
+class Unimplemented(Exception):
+    pass
+
+class MeshManager:
+    # TODO: Do I even need a connected variable here? See if there is one in the interface object
+    connected = False  # TODO: Hey, this forces a single device at a time. Fix that!
+    supported_interface_types = ["ble", "tcp", "serial"]
+    # An "interface" is (connected, meshtastic.interface object)
+    interfaces = []  # TODO: Add interface on first connect, making a distinction between "active" and "connected"
+
+    def __init__(self):
+        super().__init__()
+        logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+        pub.subscribe(self.onConnectionUp, "meshtastic.connection.established")
+        pub.subscribe(self.onConnectionDown, "meshtastic.connection.lost")
+
+    def onConnectionUp(self, interface, topic=pub.AUTO_TOPIC):
+        self.connected = True  # TODO: fix for multiple interfaces
+        return
+
+    def onConnectionDown(self, interface, topic=pub.AUTO_TOPIC):
+        self.connected = False  # TODO: fix for multiple interfaces
+        return
+
+    def find_all_available_devices(self, interface_types=None) -> list:
+        # Find all available mesh nodes on the listed interface types
+        # Returns a list of tuples: (type, address, name)
+
+        if interface_types is None:
+            interface_types = self.supported_interface_types
+        else:
+            if not isinstance(interface_types, list):
+                raise InterfaceError("interface_type must be a list")
+            if not all(y in self.supported_interface_types for y in interface_types):
+                raise InterfaceError(f"interface_type must be one of {str(self.supported_interface_types)}")
+
+        # For each interface type, find all devices
+        devices = []  # List of all available devices on all requested types
+        for i_type in interface_types:
+            devs = self.find_devices_on_type(i_type)
+            devices.extend(devs)
+
+        return devices
+
+    def find_devices_on_type(self, interface_type: str) -> list:
+        # Find all available devices on a specific interface type
+        # Returns a list of tuples: (type, address, name)
+
+        if not all(y in self.supported_interface_types for y in interface_type):
+            raise InterfaceError("interface_type must be one of 'ble', 'tcp', or 'serial'")
+
+        if interface_type == "ble":
+            return ble.scan_all_devices()  # Gives us a list of ("ble", address, name) tuples
+        elif interface_type == "tcp":
+            raise Unimplemented("TCP interface type is not currently supported")
+        elif interface_type == "serial":
+            raise Unimplemented("Serial interface type is not currently supported")
+        else:
+            raise InterfaceError("Unknown interface type")  # Should not reach this (belt and suspenders)
+
+    def connect_to_specific_device(self, address):  # TODO: verify input(s)
+        # TODO: Don;'t forget to add the interface if it doesn't exist yet
+        pass
+
+    def disconnect_from_specific_device(self, address):
+        pass
+
+    def connect_to_first_device_on_type(self, interface_type: str):
+        pass
+
+    def connect_to_first_available_device(self, type_list:list):
+        pass
