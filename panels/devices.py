1302017b5bcf7314e9f16b7f7cc94c4452599645
ab8oj <e.e.thierbach@gmail.com>
2025-09-06 18:14:15 -0400

    Device page UI updates

--- panels/devices.py
+++ panels/devices.py
@@ -8,57 +8,53 @@ class DevicesPanel(wx.Panel):
     def __init__(self, parent):
         wx.Panel.__init__(self, parent, wx.ID_ANY)
 
-        splitter = wx.SplitterWindow(self, wx.ID_ANY)
-        top_panel = TopPanel(splitter)
-        bottom_panel = BottomPanel(splitter)
-        splitter.SplitHorizontally(top_panel, bottom_panel)
-        splitter.SetMinimumPaneSize(20)
+        sizer = wx.BoxSizer(wx.VERTICAL)  # outer box
+        device_box = wx.BoxSizer(wx.HORIZONTAL)  # Top part: buttons and device list
 
-        sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(splitter, 1, wx.EXPAND)
-        self.SetSizer(sizer)
-
-
-class TopPanel(wx.Panel):
-    def __init__(self, parent):
-        wx.Panel.__init__(self, parent, wx.ID_ANY)
-        device_box = wx.BoxSizer(wx.HORIZONTAL)
-
-        device_button_box = wx.BoxSizer(wx.VERTICAL)  # Left side of device box: buttons that do things
+        device_button_box = wx.BoxSizer(wx.VERTICAL)  # Left side of device box: buttons
         discover_button = wx.Button(self, wx.ID_ANY, "Discover")
         self.discover_ble = wx.CheckBox(self, wx.ID_ANY, "Discover BLE")
         self.discover_serial = wx.CheckBox(self, wx.ID_ANY, "Discover Serial")
         self.discover_tcp = wx.CheckBox(self, wx.ID_ANY, "Discover TCP")
-        connect_button = wx.Button(self, wx.ID_ANY, "Connect")
-        disconnect_button = wx.Button(self, wx.ID_ANY, "Disconnect")
+        self.connect_button = wx.Button(self, wx.ID_ANY, "Connect")
+        self.disconnect_button = wx.Button(self, wx.ID_ANY, "Disconnect")
+        self.connect_button.Disable()
+        self.disconnect_button.Disable()
         self.Bind(wx.EVT_BUTTON, self.onDiscoverButton, discover_button)
-        self.Bind(wx.EVT_BUTTON, self.onConnectButton, connect_button)
-        self.Bind(wx.EVT_BUTTON, self.onDisconnectButton, disconnect_button)
+        self.Bind(wx.EVT_BUTTON, self.onConnectButton, self.connect_button)
+        self.Bind(wx.EVT_BUTTON, self.onDisconnectButton, self.disconnect_button)
         device_button_box.Add(discover_button)
         device_button_box.Add(self.discover_ble)
         device_button_box.Add(self.discover_serial)
         device_button_box.Add(self.discover_tcp)
-        device_button_box.Add(connect_button)
-        device_button_box.Add(disconnect_button)
+        device_button_box.Add(self.connect_button)
+        device_button_box.Add(self.disconnect_button)
         device_box.Add(device_button_box)
 
-        # TODO: Make the device list box expand to the right as frame expands
         device_list_box = wx.BoxSizer(wx.VERTICAL)  # Right side of device box: device list
         self.device_list = wx.ListCtrl(self, style=wx.LC_REPORT | wx.LC_SINGLE_SEL)
         self.device_list.InsertColumn(0, 'Name')
         self.device_list.InsertColumn(1, 'Status')
         self.device_list.InsertColumn(2, 'Type')
         self.device_list.InsertColumn(3, 'Address')
-        self.device_list_index = 0  # Start with 0 items -- do I really need this?
+        self.Bind(wx.EVT_LIST_ITEM_SELECTED, self.onDeviceSelected, self.device_list)
+        self.Bind(wx.EVT_LIST_ITEM_DESELECTED, self.onDeviceDeselected, self.device_list)
         device_list_box.Add(self.device_list, 1, flag=wx.EXPAND)
-        device_box.Add(device_list_box)
+        device_box.Add(device_list_box, 1, flag=wx.EXPAND)
 
-        self.SetSizer(device_box)
+        device_details_box = wx.BoxSizer(wx.HORIZONTAL)  # Bottom part: device information
+        self.temp_text = wx.TextCtrl(self, wx.ID_ANY, "Device information goes here",
+                                     style=wx.TE_READONLY)
+        device_details_box.Add(self.temp_text, 1, flag=wx.EXPAND)
+
+        sizer.Add(device_box, 0, flag=wx.EXPAND)
+        sizer.Add(device_details_box, 0, flag=wx.EXPAND)
+
+        self.SetSizer(sizer)
         self.SetAutoLayout(True)
         device_box.Fit(self)
 
         self.device_manager = DeviceManager()
-
         pub.subscribe(self.onConnectionUp, "meshtastic.connection.established")
         pub.subscribe(self.onConnectionDown, "meshtastic.connection.lost")
 
@@ -90,18 +86,14 @@ class TopPanel(wx.Panel):
         # TODO: How will the split of name work out for serial and TCP devices?
         for dev_type, address, name in discovered_devices:
             self.device_list.Append((name.split("_")[0], "Disconnected", dev_type, address))
-            self.device_list_index = +1
-            # TODO: Change explicit columns to "for x in range(number_of_columns)
-            self.device_list.SetColumnWidth(0, wx.LIST_AUTOSIZE)
-            self.device_list.SetColumnWidth(1, wx.LIST_AUTOSIZE)
-            self.device_list.SetColumnWidth(2, wx.LIST_AUTOSIZE)
-            self.device_list.SetColumnWidth(3, wx.LIST_AUTOSIZE)
+            for col in range(self.device_list.GetColumnCount()):
+                self.device_list.SetColumnWidth(col, wx.LIST_AUTOSIZE)
+            self.Layout()  # Columns don't resize until window is jiggled
 
         return
 
     # noinspection PyUnusedLocal
     def onConnectButton(self, event):
-        # TODO: Can connect button be disabled until a device is highlighted?
         # TODO: Deal with messages to stdout from mesh manager
         # TODO: Disallow reconnection to already-connected device (and handle failed disconnect somehow)
         selected_item = self.device_list.GetFirstSelected()
@@ -117,6 +109,8 @@ class TopPanel(wx.Panel):
             wx.RichMessageDialog(self, f"Error connecting to device: {str(e)}",
                                  style=wx.OK | wx.ICON_ERROR).ShowModal()
             return
+
+        self.temp_text.SetLabel("Connection pending")
 
         return
 
@@ -136,36 +130,41 @@ class TopPanel(wx.Panel):
             wx.RichMessageDialog(self, f"Error disconnecting from device: {str(e)}",)
             return
 
+    # noinspection PyUnusedLocal
+    def onDeviceSelected(self, event):
+        self.connect_button.Enable()
+        self.disconnect_button.Enable()
+        return
 
+    # noinspection PyUnusedLocal
+    def onDeviceDeselected(self, event):
+        self.connect_button.Disable()
+        self.disconnect_button.Disable()
+
+    # noinspection PyUnusedLocal
     def onConnectionUp(self, interface):
+        # TODO: Connection status does not update until window is manipulated
+        # TODO: Trying to do layout() in here or "widget changed" handler crashes w/thread error
         short_name = interface.getShortName()
-        message = f"Connection established on interface {short_name}"
-        pub.sendMessage("mainframe.changeStatusBar", status_text=message)
         index = self.device_list.FindItem(-1, short_name)
         if index == -1:
             message = (f"WARNING: Device with address {short_name} was not found in the node list, "
                        f"connection status cannot be updated")
-            pub.sendMessage("mainframe.changeStatusBar", status_text=message)
+            wx.RichMessageDialog(self, message, style=wx.ICON_WARNING).ShowModal()
         else:
             self.device_list.SetItem(index, 1, "Connected")
+        self.temp_text.SetLabel("Device informtion still goes here ")
+
         return
 
     def onConnectionDown(self, interface):
         # TODO: Turning off a BLE device doesn't seem to publish to this topic. Why?
         short_name = interface.getShortName()
-        message = f"Connection lost on interface {short_name}"
-        pub.sendMessage("mainframe.changeStatusBar", status_text=message)
         index = self.device_list.FindItem(-1, short_name)
         if index == -1:
             message = (f"WARNING: Device with address {short_name} was not found in the node list, "
                        f"connection status cannot be updated")
-            pub.sendMessage("mainframe.changeStatusBar", status_text=message)
+            wx.RichMessageDialog(self, message, style=wx.ICON_WARNING).ShowModal()
         else:
             self.device_list.SetItem(index, 1, "Disconnected")
         return
-
-
-class BottomPanel(wx.Panel):
-    def __init__(self, parent):
-        wx.Panel.__init__(self, parent, wx.ID_ANY)
-        wx.StaticText(self, wx.ID_ANY, "Device details go here")
