7fef9515946eca68ff4f86df7e988850413b4658
ab8oj <e.e.thierbach@gmail.com>
2025-11-07 21:29:29 -0500

    Add logging to tcp.py

--- common/tcp.py
+++ common/tcp.py
@@ -1,33 +1,36 @@
 # Interface to a node connected via tcp
+import logging
 
 import meshtastic.tcp_interface
 import meshtastic.util
 
 from gui import shared
 
-
-# TODO: it would be nice to make this class-based somehow.
-# TODO: change print statements to logger
+log = logging.getLogger(__name__)
+# log.setLevel(logging.DEBUG)  # Set our own level separately
 
 def make_connection_and_return(address):
     # This relies on the caller to keep the script alive, and to close the interface
-
+    log.info(f"Connecting to {address}")
     interface = None
     try:
         interface = meshtastic.tcp_interface.TCPInterface(address)
-        print(f"TCP interface to {interface.getShortName()} initialized")
+        log.info(f"TCP interface to {interface.getShortName()} initialized")
     except Exception as e:
-        print(f"\nERROR: Could not connect via TCP: {e}")
+        log.info(f"\nERROR: Could not connect via TCP: {e}")
 
     return interface
 
 def scan_all_devices() -> list:
     # Return a list of tuples: ("tcp", device address, device name (address again)) or empty list
     if "TCP_DEVICES" not in shared.config:
+        log.warning("No TCP_DEVICES key found in configuration file")
         return []
 
+    log.info("Scanning for TCP devices")
     dev_list = []
     for dev in shared.config["TCP_DEVICES"].split(","):
         # TODO: basic IP address / hostname sanity checking
         dev_list.append(("tcp", dev, dev))  # Return the device address or DNS hostname for both address and shortname
+    log.info(f"Found {len(dev_list)} TCP devices")
     return dev_list
