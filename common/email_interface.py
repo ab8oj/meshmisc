18cfd9c56aa452a8d23f6003ec64adfd0e99ed04
ab8oj <e.e.thierbach@gmail.com>
2025-11-07 21:14:00 -0500

    Add logging to email_interface.py

--- common/email_interface.py
+++ common/email_interface.py
@@ -1,8 +1,11 @@
 # Send email using an external SMTP server
-# TODO: Move SMTP .env data here and simplify send_email parameters
 
+import logging
 import smtplib
 from email.message import EmailMessage
+
+log = logging.getLogger(__name__)
+# log.setLevel(logging.DEBUG)  # Set our own level separately
 
 def send_email(smtp_server, sender, password, from_address, to_address, subject, body):
     msg = EmailMessage()
@@ -12,6 +15,13 @@ def send_email(smtp_server, sender, password, from_address, to_address, subject,
     msg['To'] = to_address
 
     # Send the message via SMTP over SSL connection
+    log.info(f"Sending email to {to_address}")
     with smtplib.SMTP_SSL(smtp_server, 465) as smtp_server:
-        smtp_server.login(sender, password)
-        smtp_server.sendmail(from_address, to_address, msg.as_string())
+        try:
+            smtp_server.login(sender, password)
+            smtp_server.sendmail(from_address, to_address, msg.as_string())
+        except Exception as e:
+            log.error(f"Error sending email to {to_address}")
+            log.error(e)
+        else:
+            log.info(f"Successfully sent email to {to_address}")
